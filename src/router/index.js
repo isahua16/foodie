d4174d76d367b33d9a688e3365f6ef28898a0f55
isahua16 <isaelhuard@gmail.com>
2023-04-25 17:24:24 -0600

    client sign up system is implemented

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import ClientHomePage from '@/views/ClientHomePage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: `/`,
+    component: ClientHomePage
+  },
 
 
 ]
---
+++ src/views/ClientHomePage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <client-sign-up></client-sign-up>
+    </div>
+</template>
+
+<script>
+import ClientSignUp from '@/components/ClientSignUp.vue'
+
+    export default {
+        components: {
+            ClientSignUp,
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    div {
+        display: grid;
+        place-items: center;
+    }
+</style>
