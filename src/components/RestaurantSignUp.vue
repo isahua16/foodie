3af30e8c6d0f40d991dbcd0220bcbc5da9c338c4
isahua16 <isaelhuard@gmail.com>
2023-04-27 20:07:01 -0600

    created the restaurant login component

--- src/components/RestaurantSignUp.vue
+++ src/components/RestaurantSignUp.vue
@@ -1,15 +1,78 @@
 <template>
-    <div>
-
-    </div>
+        <section>
+            <h2>Restaurant Sign Up</h2>
+            <h4>{{error_message}}</h4>
+            <input placeholder="Email" type="text" ref="input_email">
+            <input placeholder="Name" type="text" ref="input_name">
+            <input placeholder="Address" type="text" ref="input_address">
+            <input placeholder="Phone Number" type="text" ref="input_phone_number">
+            <input placeholder="Bio" type="text"  ref="input_bio">
+            <select name="restaurant_sign_up" ref="input_city">
+                <option value="Vancouver">Vancouver</option>
+                <option value="Calgary">Calgary</option>
+                <option value="Toronto">Toronto</option>
+            </select>
+            <input placeholder="Profile Image URL" type="text" ref="input_profile_url">
+            <input placeholder="Banner Image URL" type="text" ref="input_banner_url">
+            <input placeholder="Password" type="password" ref="input_password">
+            <button @click="handle_restaurant_signup">Sign Up</button>
+        </section>
 </template>
 
 <script>
+import axios from 'axios'
+import cookies from 'vue-cookies'
     export default {
+        methods: {
+            handle_restaurant_signup: function() {
+                this.error_message = undefined;
+                axios.request(
+                    {
+                        url: `https://foodie.bymoen.codes/api/restaurant`,
+                        method: `POST`,
+                        headers: {
+                            'x-api-key': `9uOwrHiuKE6VUs8CIbJo`
+                        },
+                        data: {
+                            email: this.$refs[`input_email`].value,
+                            name: this.$refs[`input_name`].value,
+                            address: this.$refs[`input_address`].value,
+                            phone_number: this.$refs[`input_phone_number`].value,
+                            bio: this.$refs[`input_bio`].value,
+                            city: this.$refs[`input_city`].value,
+                            profile_url: this.$refs[`input_profile_url`].value,
+                            banner_url: this.$refs[`input_banner_url`].value,
+                            password: this.$refs[`input_password`].value
+                        }
+                    }
+                ).then((res) => {
+                    console.log(res);
+                    cookies.set(`restaurant_id`, JSON.stringify(res.data.restaurantId));
+                    cookies.set(`token`, res.data.token);
+                    // this.$router.push(`/restaurant-profile`);
+                }).catch((err) => {
+                    console.log(err);
+                    this.error_message = `An error occured. Try again`;
+                });
+            }
+        },
+        data() {
+            return {
+                error_message: undefined
+            }
+        },
         
     }
 </script>
 
 <style lang="scss" scoped>
+    section {
+        display: grid;
+        width: 80%;
+        place-items: center;
+    }
 
+    select {
+        width:50%;
+    }
 </style>
