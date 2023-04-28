3b53c814736c5489daa98baf5677defd12a6d154
isahua16 <isaelhuard@gmail.com>
2023-04-27 21:56:37 -0600

    Readded the Restaurant profile page that I deleted by mistake

---
+++ src/views/RestaurantProfilePage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <restaurant-header></restaurant-header>
+        <restaurant-edit-profile></restaurant-edit-profile>
+        <restaurant-delete-account></restaurant-delete-account>
+    </div>
+</template>
+
+<script>
+import RestaurantHeader from '@/components/RestaurantHeader.vue'
+import RestaurantEditProfile from '@/components/RestaurantEditProfile.vue'
+import RestaurantDeleteAccount from '@/components/RestaurantDeleteAccount.vue'
+    export default {
+        components: {
+            RestaurantEditProfile,
+            RestaurantHeader,
+            RestaurantDeleteAccount
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    div {
+        display: grid;
+        place-items: center;
+    }
+</style>
